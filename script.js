1fbc9a2bea4b8650913bb6339eaec73e712085a3
vitellium <vitelliumluxuryegg@gmail.com>
2026-03-24 03:51:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Vitellium loaded');
+const fades = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+  fades.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('visible');
+    }
+  });
+});
